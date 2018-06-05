f98a95e20057d93eea7516723d8903f4a3bdc1b4
Uladzimir Aleshka <uladzimir.aleshka@itechart-group.com>
2018-06-05 17:28:46 +0300

    Added validation example

--- src/todo/todo.model.ts
+++ src/todo/todo.model.ts
@@ -1,10 +1,11 @@
-import { Table, Column, Model } from 'sequelize-typescript';
+import { Table, Column, Model, Length } from 'sequelize-typescript';
 
 @Table
 export default class Todo extends Model<Todo> {
     @Column({})
     title: string;
 
+    @Length({ min: 3, max: 10 })
     @Column({})
     text: string;
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,15 +1,26 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ValidationError } from 'sequelize';
 
 import Todo from './todo.model';
 import { Symbols } from '../constants';
+import { AppLogger } from '../logger';
 
 @Injectable()
 export class TodoService {
+    private logger = AppLogger.getLogger();
+
     public constructor(
-        @Inject(Symbols.Repository.Todo) private readonly todoRepository: typeof Todo,
+        @Inject(Symbols.Repository.Todo)
+        private readonly todoRepository: typeof Todo,
     ) {}
 
     public async findAll(): Promise<Todo[]> {
+        try {
+            await this.todoRepository.create({ title: 'x', text: 'a' });
+        } catch (e) {
+            this.logger.warn(`${e instanceof ValidationError}`);
+        }
+
         return await this.todoRepository.findAll<Todo>();
     }
 }
